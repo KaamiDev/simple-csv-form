491ec421b83ec3c5cfae9051076eaa48166a08a4
Kaami Dev <kaami@kaamidev.com>
2020-09-19 18:44:19 -0400

    added comments

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// setup routes
+// homepage route
 app.get('/', (req, res) => {
+	// renders view sending query parameter
 	res.render('index.ejs', { status: req.query.status });
 });
 
+// submit route
 app.post('/submit', (req, res) => {
+	// makes sure no fields are missing
 	if (req.body.name && req.body.surname && req.body.email) {
+		// creates data array
 		let data = [
 			{
 				name: req.body.name,
@@ -27,13 +31,15 @@ app.post('/submit', (req, res) => {
 				email: req.body.email
 			}
 		];
-
+		// writes data array to csv file, appending existing data
 		new ObjectsToCsv(data).toDisk('./data.csv', {
 			append: true
 		});
 
+		// redirects to submitted route
 		res.redirect('/?status=submitted');
 	} else {
+		// if fields missing, redirects back
 		res.redirect('back');
 	}
 });
